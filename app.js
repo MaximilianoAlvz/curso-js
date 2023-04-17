e4967f41ac28940d5d5e5360bf01a9ac206fb4e1
Maximiliano <mavzdev@gmail.com>
2023-04-16 21:22:43 -0300

    se agrego un if y switch

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ while (edad < 18){
 
 alert("Bienvenido" + " " + nombre);
 
+let reintentar;
+let verGraduacion;
+let graduacionBebida
 let continuar;
 let nombreBebida;
 let totalBebida = "";
@@ -14,7 +17,40 @@ let totalBebida = "";
 while(continuar != "no" ) {
 
     nombreBebida = prompt("que bebida desea agregar?");
-    totalBebida = totalBebida + ", " + nombreBebida;
+    totalBebida = totalBebida + nombreBebida + "\n" ;
     continuar = prompt("¿desea agregar otra bebida? si/no").toLowerCase();
 }
-alert("Agrego las siguientes bebidas" + totalBebida);
+alert("Agrego las siguientes bebidas: " + "\n" + totalBebida);
+
+verGraduacion = prompt("¿Desea consultar la graduacion alcoholica de algunas bebibas? Si/No").toLowerCase();
+
+if (verGraduacion == "si") {
+    do {
+    graduacionBebida = Number(prompt("¿de cual de las siguientes bebidas desea consultar la graduacion?" + "\n" + 
+    "1 - Vodka" + "\n" +
+    "2 - Fernet" + "\n" +
+    "3 - Gancia" + "\n" +
+    "4 - Vino" + "\n" +
+    "5 - Ron" + "\n"))
+
+        switch (graduacionBebida) {
+            case 1: alert("La graduacion alcoholica del Vodka es del 40%")
+            repetir = prompt("¿Desea consultar alguna otra bebida? Si/No").toLowerCase()
+            break;
+            case 2: alert ("La graduacion alcoholica del fernet varia entre un 39% a 45% segun la marca")
+            repetir = prompt("¿Desea consultar alguna otra bebida? Si/No").toLowerCase()
+            break;
+            case 3: alert ("La graduacion alcoholica del Gancia es de un 30%")
+            repetir = prompt("¿Desea consultar alguna otra bebida? Si/No").toLowerCase()
+            break;
+            case 4: alert ("La graduacion alcoholica del vino varia entre un 10% a 14% segun la marca")
+            repetir = prompt("¿Desea consultar alguna otra bebida? Si/No").toLowerCase()
+            break;
+            case 5: alert ("La graduacion alcoholica del Ron varia entre un 35% a 54% segun la marca")
+            repetir = prompt("¿Desea consultar alguna otra bebida? Si/No").toLowerCase()
+        }}
+        while (repetir == "si");
+        alert ("¡" + nombre + " gracias por visitarnos!");
+} else {
+    alert ("¡" + nombre + " gracias por visitarnos!");
+}
